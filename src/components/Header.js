98e4a70889b926d46a9e28c54a793bbd713c5595
Luis Quinones <alexx813@gmail.com>
2017-10-21 18:37:42 -0500

    added totals feauture

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <h1>Glocal</h1>
+    <h1>Expensify</h1>
     <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Add Client</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
   </header>
 );
 
